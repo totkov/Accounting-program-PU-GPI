657db976dc52bbd139ddc6d186a2512f0ebddf18
totkov <neznamue@gmail.com>
2016-02-23 17:11:16 +0200

    Added User.cs

---
+++ Source/Tiamat.WindowsForms/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiamat.WindowsForms
+{
+   public class User
+    
+   {
+
+       public User()
+       {
+
+       }
+
+       public User(string user,string pass)
+       {
+           this.Username = user;
+           this.Password = pass;
+       }
+
+       public User(string user, string pass, string email,string fname, string lname)
+       {
+           this.Username = user;
+           this.Password = pass;
+           this.Email = email;
+           this.FirstName = fname;
+           this.LastName = lname;
+       }
+       
+
+
+       
+
+       public string Username { get; set; }
+       public string Password { get; set; }
+       public string Email{get;set;}
+       public string FirstName { get; set; }
+       public string LastName { get; set; }
+
+    }
+}
